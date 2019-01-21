715277a46786d8e6dd7a665aa65860ae48d1840a
Subangkar-GitKraken-Win10-HP <subangkar.karmaker@gmail.com>
2019-01-21 19:52:18 +0600

    Updated readme
    -> user prog has been made simple

---
+++ README.md
@@ -0,0 +1,61 @@
+Socket Application Programming Interface (API) implementation in [xv6 Operating System of MIT](https://github.com/mit-pdos/xv6-public) which extends its functionality
+to support localhost networking.  
+  
+System calls (added):
+------------------------------- 
+  - **listen (localPort)**  
+    This is for a server to listen to a local port for accepting client socket. Returns 0 for success, negative for failure.
+    
+  - **connect (remotePort, hostname)**  
+    This is for a client to connect to remote port and host. **Only "localhost" and "127.0.0.1" as host**
+    should be supported. Returns the local port number, if connection was successful. Negative
+    values returned indicate failure.
+    
+  - **send (localPort, buffer, length)**  
+    Sends a buffer to remote host connected to given localPort. Returns 0 for success. Negative values for failure. send() blocks, if remote host has not yet read earlier
+    data
+  
+  - **recv (localPort, buffer, length)**  
+    Receives data from remote host connected to given localPort. Returns 0 for success. Negative values for failure. recv() blocks, if no data is available.
+  
+  - **disconnect (localPort)**  
+    Disconnects (and closes) the socket at given port number.
+  
+  
+Limitations :
+--------------------------------
+- a server can connect to only one client at a time
+- send() or recv() will block the caller process, until the recipients buffer is ready.
+- timeout cannot be specified in the listen() or recv() call
+
+Demonstration:
+--------------------------------
+- run this xv6 repo in qemu and a user program will show up in `socktest`  
+	> make qemu
+- in xv6 shell	
+	> ls
+	
+	> socktest
+	
+`socktest` user program demonstrates a simple client-server interaction where client takes user input from console and server echoes it back to client. If any error occurs corresponding error codes is returned.  
+
+Error codes:
+-------------------------------
+_`E_NOTFOUND -1025`  
+`E_ACCESS_DENIED -1026`  
+`E_WRONG_STATE -1027`  
+`E_FAIL -1028`  
+`E_INVALID_ARG -1029`  
+`E_CONNECTION_RESET_BY_REMOTE -1030`_
+
+Parameter issues would return `E_INVALID_ARG`.  
+Accessing a socket that is not in the stable then `E_NOTFOUND`.  
+Accessing a socket from wrong process then `E_ACCESS_DENIED`.  
+Attempts to send or receive, when the socket is not connected, then
+`E_WRONG_STATE`.  
+If no more socket can be opened (limit exceeded) `E_FAIL`.  
+If connection is reset during send or receive then `E_CONNECTION_RESET_BY_REMOTE`
+  
+*Error codes are defined in "signalcodes.h"*
+  
+This xv6 networking extension has been performed as an assignment of Operating System Sessional course in Level-3, Term-2 of Department of CSE, BUET.
--- socktest.c
+++ socktest.c
@@ -2,87 +2,86 @@
 #include "user.h"
 #include "signalcodes.h"
 
-int serverPort = 10;
-
 void clientProc(int serverPort) {
-  int clientPort;
-  char buf[128];
-  char host[16] = "localhost";
+	int clientPort;
+	char buf[128];
+	char host[16] = "localhost";
 
-  // sleep for 100 clock ticks to ensure that the server process starts first.
-  sleep(100);
+	// sleep for 100 clock ticks to ensure that the server process starts first.
+	sleep(100);
 
-  printf(1, "Client>> Attempting to connect to port %d, host %s ...\n", serverPort, host);
-  clientPort = connect(serverPort, host);
-  sleep(20);
-  printf(1, "%d Client>> connect() returned %d\n", clientPort);
-  while (1) {
-    printf(1, "%d Client>> Enter text to send to server: ", clientPort);
-    gets(buf, sizeof(buf));
-    buf[strlen(buf) - 1] = '\0'; // Eliminating the '\n'
-    printf(1, "%d Client>> to send to server: %s\n", clientPort, buf);
-    int ret = send(clientPort, buf, strlen(buf) + 1);
+	printf(1, "Client>> Attempting to connect to port %d, host %s ...\n", serverPort, host);
+	clientPort = connect(serverPort, host);
+	sleep(20);
+	printf(1, "%d Client>> connect() returned %d\n", clientPort);
+	while (1) {
+		printf(1, "%d Client>> Enter text to send to server: ", clientPort);
+		gets(buf, sizeof(buf));
+		buf[strlen(buf) - 1] = '\0'; // Eliminating the '\n'
+		printf(1, "%d Client>> to send to server: %s\n", clientPort, buf);
+		int ret = send(clientPort, buf, strlen(buf) + 1);
 
-    if (ret < 0 || 0 == strcmp(buf, "exit")) {
-      printf(1, "%d Client exiting...(%d)\n", clientPort, ret);
-      disconnect(clientPort);
-      break;
-    }
+		if (ret < 0 || 0 == strcmp(buf, "exit")) {
+			printf(1, "%d Client exiting...(%d)\n", clientPort, ret);
+			disconnect(clientPort);
+			break;
+		}
 
-    sleep(100 + uptime() % 100);
+		sleep(100 + uptime() % 100);
 
-    recv(clientPort, buf, sizeof(buf));
-    printf(1, "%d Client>> Received: \"%s\"\n", clientPort, buf);
-  }
+		recv(clientPort, buf, sizeof(buf));
+		printf(1, "%d Client>> Received: \"%s\"\n", clientPort, buf);
+	}
 }
 
 void serverProc(int serverPort) {
-  int status;
-  char buf[128];
+	int status;
+	char buf[128];
 
-  printf(1, "Server>> Starting to listen at port %d ...\n", serverPort);
-  status = listen(serverPort);
-  printf(1, "%d Server>> listen() returned %d\n", serverPort, status);
+	printf(1, "Server>> Starting to listen at port %d ...\n", serverPort);
+	status = listen(serverPort);
+	printf(1, "%d Server>> listen() returned %d\n", serverPort, status);
 
-  while (1) {
-    sleep(100 + uptime() % 100);
+	while (1) {
+		sleep(100 + uptime() % 100);
 
-    int ret = recv(serverPort, buf, sizeof(buf));
-    printf(1, "%d Server>> Received: \"%s\"\n",serverPort , buf);
+		int ret = recv(serverPort, buf, sizeof(buf));
+		printf(1, "%d Server>> Received: \"%s\"\n", serverPort, buf);
 
-    if (ret < 0 || 0 == strcmp(buf, "exit")) {
-      printf(1, "%d Server exiting...(%d)\n",serverPort , ret);
-      disconnect(serverPort);
-      break;
-    }
+		if (ret < 0 || 0 == strcmp(buf, "exit")) {
+			printf(1, "%d Server exiting...(%d)\n", serverPort, ret);
+			disconnect(serverPort);
+			break;
+		}
 
-    sleep(100 + uptime() % 100);
+		sleep(100 + uptime() % 100);
 
-    strcpy(buf+strlen(buf), " OK");
-    send(serverPort, buf, strlen(buf) + 1);
-  }
+		strcpy(buf + strlen(buf), " OK");
+		send(serverPort, buf, strlen(buf) + 1);
+	}
 }
 
 void sockLimitTest();
 
-int main(int argc, char *argv[])
-{
-	sockLimitTest();
-	exit();
-  if (0 == fork()) {
-    clientProc(serverPort);
-    exit();
-  } else {
-    serverProc(serverPort);
-    // This is the parent process. So, it needs to wait before client terminates
-    wait();
-    exit();
-  }
+int main(int argc, char *argv[]) {
+//	sockLimitTest();
+//	exit();
+	int serverPort = 10;
+
+	if (0 == fork()) {
+		clientProc(serverPort);
+		exit();
+	} else {
+		serverProc(serverPort);
+		// This is the parent process. So, it needs to wait before client terminates
+		wait();
+		exit();
+	}
 }
 
 
-
 #define N 2
+
 void client(int serverPort) {
 	int clientPort;
 	char buf[128] = "1\n";
@@ -125,34 +124,53 @@ void server(int serverPort) {
 	for (int i = 1; i <= N; i++) {
 		sleep(100 + uptime() % 100);
 		int ret = recv(serverPort, buf, sizeof(buf));
-		printf(1, "%d Server>> Received: \"%s\"\n",serverPort , buf);
+		printf(1, "%d Server>> Received: \"%s\"\n", serverPort, buf);
 
 		if (ret < 0 || 0 == strcmp(buf, "exit")) {
-			printf(1, "%d Server exiting...(%d)\n",serverPort , ret);
+			printf(1, "%d Server exiting...(%d)\n", serverPort, ret);
 			disconnect(serverPort);
 			break;
 		}
 
 		sleep(100 + uptime() % 100);
-		strcpy(buf+strlen(buf), " OK");
+		strcpy(buf + strlen(buf), " OK");
 		send(serverPort, buf, strlen(buf) + 1);
 	}
 }
 
 
 #define WAIT_TIME 200
-void sockLimitTest(){
-	if(!fork()){server(24);exit();}
+
+void sockLimitTest() {
+	if (!fork()) {
+		server(24);
+		exit();
+	}
 	sleep(WAIT_TIME);
-	if(!fork()){client(24);exit();}
+	if (!fork()) {
+		client(24);
+		exit();
+	}
 	sleep(WAIT_TIME);
-	if(!fork()){server(126);exit();}
+	if (!fork()) {
+		server(126);
+		exit();
+	}
 	sleep(WAIT_TIME);
-	if(!fork()){client(126);exit();}
-	sleep(2*WAIT_TIME);
-	if(!fork()){serverProc(120);exit();}
+	if (!fork()) {
+		client(126);
+		exit();
+	}
+	sleep(2 * WAIT_TIME);
+	if (!fork()) {
+		serverProc(120);
+		exit();
+	}
 	sleep(WAIT_TIME);
-	if(!fork()){clientProc(120);exit();}
+	if (!fork()) {
+		clientProc(120);
+		exit();
+	}
 	sleep(WAIT_TIME);
 	while (wait() > 0);
 }
